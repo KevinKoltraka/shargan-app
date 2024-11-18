ec2cfd3b31f606214175c48efb21e1bb72de4795
Kevin Koltraka <kevikoltraka157@gmail.com>
2024-11-18 17:18:16 +0100

    changes

--- src/components/Partners/Partner.js
+++ src/components/Partners/Partner.js
@@ -8,24 +8,14 @@ import Dots4 from "./BrandImages/flex.png";
 const Partner = () => {
   const scrollRef = useRef(null);
 
-  const scrollLeft = () => {
-    scrollRef.current.scrollBy({ left: -200, behavior: "smooth" });
-  };
-
-  const scrollRight = () => {
-    scrollRef.current.scrollBy({ left: 200, behavior: "smooth" });
-  };
-
   return (
     <div className="brand-partners-container">
-      <button className="arrow left-arrow" onClick={scrollLeft}>&lt;</button>
       <div className="brand-partners" ref={scrollRef}>
         <img src={Dots1} alt="brand-partners" />
         <img src={Dots2} alt="brand-partners" />
         <img src={Dots3} alt="brand-partners" />
         <img src={Dots4} alt="brand-partners" />
       </div>
-      <button className="arrow right-arrow" onClick={scrollRight}>&gt;</button>
     </div>
   );
 };

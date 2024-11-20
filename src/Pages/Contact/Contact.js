851bc26b5d5ce541fddb546e627a8a9de8b8573f
Kevin Koltraka <kevikoltraka157@gmail.com>
2024-11-20 17:29:00 +0100

    changes

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,18 +1,37 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import './contact.css';
 import ContactImg from './../../images/chat.gif';
 import Social from '../../components/SocialIcons/Social';
 import Hero from '../../components/HeroSection/Hero';
+
 const Contact = () => {
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (message) {
+      openGmailCompose(message);
+    } else {
+      alert("Please enter a message.");
+    }
+  };
+
+  const openGmailCompose = (message) => {
+    const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=info@sharganconsulting.com&body=${encodeURIComponent(message)}`;
+    const newWindow = window.open(gmailLink, '_blank');
+
+    if (!newWindow || newWindow.closed || typeof newWindow.closed === 'undefined') {
+      alert("Popup blocked! Please allow popups for this site to proceed.");
+    }
+  };
+
   return (
     <>
       <Hero
         title="Contact Us"
         description="We’d love to hear from you! Feel free to reach out with any questions, feedback, or inquiries, and we’ll get back to you as soon as possible."
         heroImg={ContactImg}
-        urlLink={
-          'https://api.whatsapp.com/send?phone=355699305604'
-        }
+        urlLink="https://api.whatsapp.com/send?phone=355699305604"
       />
 
       <div className="social-contact">
@@ -21,11 +40,7 @@ const Contact = () => {
 
       <div className="mega-contact-box">
         <div className="contact-form-wrapper">
-          <form
-            action="mailto:info@sharganconsulting.com"
-            method="post"
-            enctype="text/plain"
-          >
+          <form onSubmit={handleSubmit}>
             <div className="enquiry-form">
               <h3>Enquiry Form</h3>
             </div>
@@ -38,7 +53,12 @@ const Contact = () => {
               <label>Email:</label>
             </div>
             <div className="form-item">
-              <textarea className="" name="message" required></textarea>
+              <textarea
+                name="message"
+                required
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
               <label>Message:</label>
             </div>
             <button type="submit" value="Send" className="submit-btn">

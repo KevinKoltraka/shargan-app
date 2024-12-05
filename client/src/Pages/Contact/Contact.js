9a1e03603398d03d020b30725762ebdbc91f92bb
Kevin Koltraka <kevikoltraka157@gmail.com>
2024-12-05 12:54:50 +0100

    changes

--- client/src/Pages/Contact/Contact.js
+++ client/src/Pages/Contact/Contact.js
@@ -14,7 +14,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
   
-    const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+    const API_URL = process.env.REACT_APP_API_URL || "https://localhost:5000";
   
     try {
       const response = await fetch(`${API_URL}/send-email`, {
